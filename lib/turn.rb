c9e5b099a1db917f026170c4fdfbd063643b1b4e
Jack T <jackmt9@gmail.com>
2020-01-12 01:23:46 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,32 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(position)
+  index = position.to_i - 1
+end
+
+def move(board, position, token ="X")
+ board[position] = token
+end
+
+def position_taken?(board, index)
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  else return false
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  valid_move?(board, index)
+end
